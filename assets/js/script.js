bc5428bb53272dac1c38ec8c6267a1576ba3c8a8
Paul Chang <paulchang959@gmail.com>
2020-11-30 14:06:38 -0800

    finalized drag-drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -230,6 +230,22 @@ $(".card .list-group").sortable({
     console.log(tempArr);
   }
 });
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");  
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+  
+});
 // load tasks for the first time
 loadTasks();
 
